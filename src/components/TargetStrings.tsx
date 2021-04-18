1752de92a625d80ffd67178154f2a8420e0782be
Kai Sawamoto <kaisawamoto@gmail.com>
2021-04-18 12:32:45 -0700

    diplay only next two words

--- src/components/TargetStrings.tsx
+++ src/components/TargetStrings.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import '../styles/targetStrings.css'
+import {AppContext, } from '../contexts/AppContext'
 
 type props = {
   targets: string[],
 }
 
 export default function TargetStrings({targets, } : props) {
+  const {targetIdx} = React.useContext(AppContext)
+
+  const getNextTwo: () => string[] = () => {
+    let ret: string[] = [];
+    for (let i=0; i<2; i++) {
+      if (targetIdx + i < targets.length) {
+        ret.push(targets[targetIdx + i])
+      }
+    }
+    return ret
+  }
   return (
     <div className={`target-container`}>
-      {targets.map((target, id) => <><span key={id} className={"code"}>{target}</span><br></br></>)} 
+      {getNextTwo().map((target, id) => <><span key={id} className={"code"}>{target}</span><br></br></>)} 
     </div>
   )
 }
